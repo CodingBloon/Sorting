27f915fd78fe29709f79c612523f1f37b6a853d2
CodingBloon <fabian.mark.riedel@gmail.com>
2025-09-13 10:59:10 +0200

    Implemented Bubble Sort Algorithm

--- src/main/java/de/bloon/Main.java
+++ src/main/java/de/bloon/Main.java
@@ -1,6 +1,26 @@
 package de.bloon;
 
+import de.bloon.sorting.BubbleSort;
+import de.bloon.sorting.SortingAlgorithm;
+import de.bloon.sorting.util.SortInt;
+
 public class Main {
 
-    public static void main(String[] args) {}
+    public static void main(String[] args) {
+
+        SortInt[] ints = new SortInt[] {
+                SortInt.valueOf(450),
+                SortInt.valueOf(931),
+                SortInt.valueOf(896),
+                SortInt.valueOf(855),
+                SortInt.valueOf(535),
+        };
+
+        SortingAlgorithm<Integer> algorithm = new BubbleSort<>(ints);
+
+        SortInt[] sorted = (SortInt[]) algorithm.sort();
+        for(SortInt i : sorted)
+            System.out.println(i.getValue());
+
+    }
 }
--- src/main/java/de/bloon/sorting/BubbleSort.java
+++ src/main/java/de/bloon/sorting/BubbleSort.java
@@ -1,5 +1,30 @@
 package de.bloon.sorting;
 
-public class BubbleSort<T> {
+import de.bloon.sorting.util.Sortable;
 
+public class BubbleSort<T> extends SortingAlgorithm<T> {
+
+    public BubbleSort(Sortable<T>[] arr) {
+        super(arr);
+    }
+
+    @Override
+    public Sortable<T>[] sort() {
+        for(int i = 0; i < arr.length; i++) {
+
+            for(int j = 0; j < arr.length - 1; j++) {
+                if(arr[j+1].smallerAs(arr[j]))
+                    swap(j + 1, j);
+            }
+
+        }
+
+        return arr;
+    }
+
+    private void swap(int i, int j) {
+        Sortable<T> temp = this.arr[i];
+        this.arr[i] = this.arr[j];
+        this.arr[j] = temp;
+    }
 }
